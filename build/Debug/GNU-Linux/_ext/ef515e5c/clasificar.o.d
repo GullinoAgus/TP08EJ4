5b99f1f45789f72fe1cccbeda5a672c20f039f41
heladero70 <golinares@itba.edu.ar>
2020-10-10 18:57:42 -0300

    Esta es la version final del archivo

---
+++ build/Debug/GNU-Linux/_ext/ef515e5c/clasificar.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/_ext/ef515e5c/clasificar.o: \
+ ../../Desktop/clasificar.c ../../Desktop/clasificar.h
+
+../../Desktop/clasificar.h:
---
+++ build/Debug/GNU-Linux/_ext/ef515e5c/main.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/_ext/ef515e5c/main.o: ../../Desktop/main.c \
+ ../../Desktop/clasificar.h
+
+../../Desktop/clasificar.h:
